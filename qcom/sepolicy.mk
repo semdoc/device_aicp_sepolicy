aa1a3d496a358d08eeaad34098c5822e7c2356ba
Julian Veit <claymore1298@gmail.com>
2020-05-06 21:17:33 +0200

    aicp_sepolicy: we are AICP
    Fix typo introduced in https://gerrit.aicp-rom.com/c/AICP/device_aicp_sepolicy/+/100690
    
    Change-Id: I22ad65503ebd01544379eef269c1b940f1129fc7

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -24,5 +24,5 @@ endif
 
 ifneq ($(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/legacy-vendor
+    device/aicp/sepolicy/qcom/legacy-vendor
 endif
